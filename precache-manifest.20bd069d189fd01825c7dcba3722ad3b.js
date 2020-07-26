e863b8b81bced0db692f1608e001677423ae97a7
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-07-26 20:06:54 +0300

    Updates

---
+++ precache-manifest.20bd069d189fd01825c7dcba3722ad3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27c21c265b36be699812c6239b41666",
+    "url": "/ignat-tasks/index.html"
+  },
+  {
+    "revision": "02fa99b2ba28138754f2",
+    "url": "/ignat-tasks/static/css/main.d0fc8c53.chunk.css"
+  },
+  {
+    "revision": "c6b82160b2d372948007",
+    "url": "/ignat-tasks/static/js/2.487dfd2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ignat-tasks/static/js/2.487dfd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fa99b2ba28138754f2",
+    "url": "/ignat-tasks/static/js/main.1e217a70.chunk.js"
+  },
+  {
+    "revision": "4fa819b739c9912457b2",
+    "url": "/ignat-tasks/static/js/runtime-main.6295c8a3.js"
+  }
+]);
