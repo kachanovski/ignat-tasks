e205a8d082c41dc82f523d4daaff4797c0c34d36
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-09-04 22:43:42 +0300

    Updates

---
+++ precache-manifest.bd2584d8832fb20a3ff4649747e5bb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a3b8ee32c986d434b84f7a1360aafe",
+    "url": "/ignat-tasks/index.html"
+  },
+  {
+    "revision": "89fd61e6024fc5323dea",
+    "url": "/ignat-tasks/static/css/main.89996fb8.chunk.css"
+  },
+  {
+    "revision": "c35f9119a67020638000",
+    "url": "/ignat-tasks/static/js/2.ca862750.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ignat-tasks/static/js/2.ca862750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fd61e6024fc5323dea",
+    "url": "/ignat-tasks/static/js/main.f7fb83a8.chunk.js"
+  },
+  {
+    "revision": "4fa819b739c9912457b2",
+    "url": "/ignat-tasks/static/js/runtime-main.6295c8a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ignat-tasks/precache-manifest.092644d449e58c2ab768a401530b8247.js"
+  "/ignat-tasks/precache-manifest.bd2584d8832fb20a3ff4649747e5bb8e.js"
 );
 
 self.addEventListener('message', (event) => {
